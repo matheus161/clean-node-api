844e853fa370cf348d599f3f759adf842a7d1e3c
Matheus Lima <matheeus161@gmail.com>
2024-02-06 20:21:51 -0300

    ensure AuthUseCase calls LoadUserByEmailRepository with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,9 +1,13 @@
 const { MissingParamError } = require("../../utils/errors");
 
 class AuthUseCase {
+  constructor(loadUserByEmailRepositorySpy) {
+    this.loadUserByEmailRepositorySpy = loadUserByEmailRepositorySpy;
+  }
   async auth(email, password) {
     if (!email) throw new MissingParamError("email");
     if (!password) throw new MissingParamError("password");
+    await this.loadUserByEmailRepositorySpy.load(email);
   }
 }
 
@@ -20,5 +24,17 @@ describe("Auth UseCase", () => {
     const sut = new AuthUseCase();
     const promise = sut.auth("any_email@mail.com");
     expect(promise).rejects.toThrow(new MissingParamError("password"));
+  });
+
+  test("Should call LoadUserByEmailRepository with correct email", async () => {
+    class LoadUserByEmailRepositorySpy {
+      async load(email) {
+        this.email = email;
+      }
+    }
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy();
+    const sut = new AuthUseCase(loadUserByEmailRepositorySpy);
+    await sut.auth("any_email@mail.com", "any_password");
+    expect(loadUserByEmailRepositorySpy.email).toBe("any_email@mail.com");
   });
 });
