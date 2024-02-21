0570d9d26d0f877569d696e210897f7ff3c27f56
Matheus Lima <matheeus161@gmail.com>
2024-02-21 10:38:39 -0300

    move TokenGenerator to its own file

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,21 +1,6 @@
 const jwt = require("jsonwebtoken");
 const MissingParamError = require("../errors/missing-param-error");
-
-class TokenGenerator {
-  // O correto é injetar no main a var de ambiente
-  constructor(secret) {
-    this.secret = secret;
-  }
-  async generate(id) {
-    if (!this.secret) {
-      throw new MissingParamError("secret");
-    }
-    if (!id) {
-      throw new MissingParamError("id");
-    }
-    return jwt.sign(id, this.secret);
-  }
-}
+const TokenGenerator = require("./token-generator");
 
 const makeSut = () => {
   return new TokenGenerator("secret");
