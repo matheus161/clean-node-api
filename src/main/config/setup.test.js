bd43a9139cf48e733a5c40595d3ed1516da8e6cf
Matheus Lima <matheeus161@gmail.com>
2024-03-06 18:44:52 -0300

    enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -7,6 +7,16 @@ describe("App Setup", () => {
       res.send("");
     });
     const res = await request(app).get("/test_x_powered_by");
-    expect(res.headers["x-powered-by "]).toBeUndefined();
+    expect(res.headers["x-powered-by"]).toBeUndefined();
+  });
+
+  test("Should enable cors", async () => {
+    app.get("/test_cors", (req, res) => {
+      res.send("");
+    });
+    const res = await request(app).get("/test_cors");
+    expect(res.headers["access-control-allow-origin"]).toBe("*");
+    expect(res.headers["access-control-allow-method"]).toBe("*");
+    expect(res.headers["access-control-allow-headers"]).toBe("*");
   });
 });
