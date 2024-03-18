7f5124332c238800796a38e64fa3db98b3b7e2d4
Matheus Lima <matheeus161@gmail.com>
2024-03-18 19:25:11 -0300

    add login route integration test to ensure 401 response when invalid credentials are provided

--- src/main/routes/login-routes.test.js
+++ src/main/routes/login-routes.test.js
@@ -23,6 +23,7 @@ describe("Login Routes", () => {
       email: "valid_email@mail.com",
       password: bcrypt.hashSync("hashed_password", 10),
     });
+    console.log(bcrypt.hashSync("hashed_password", 10));
     await request(app)
       .post("/api/login")
       .send({
@@ -30,5 +31,15 @@ describe("Login Routes", () => {
         password: "hashed_password",
       })
       .expect(200);
+  });
+
+  test("Should return 401 when invalid credentials are provided", async () => {
+    await request(app)
+      .post("/api/login")
+      .send({
+        email: "valid_email@mail.com",
+        password: "hashed_password",
+      })
+      .expect(401);
   });
 });
